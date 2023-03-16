57e9d5dceb95b60d47e972c73d67f2b74793179f
v3adimir <vgkozlov@tut.by>
2023-03-16 11:03:41 +0300

    update

--- lab4/src/App.java
+++ lab4/src/App.java
@@ -1,5 +1,13 @@
 public class App {
+
     public static void main(String[] args) {
+        Student user = new Student();
+        user.getGroup();
+        func();
+    }
+
+    public static void func() {
         System.out.println("Hello Java!");
+
     }
 }
---
+++ lab4/src/Student.java
@@ -0,0 +1,11 @@
+public class Student extends User {
+    private int group = 1;
+
+    public int getGroup() {
+        return group;
+    }
+
+    public void setGroup(int group) {
+        this.group = group;
+    }
+}
